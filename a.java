6df55385656c7393c94550bf7744fc2d6598392b
yihong45 <93572293+yihong45@users.noreply.github.com>
2021-11-09 16:27:43 +0800

    Add files via upload

---
+++ a.java
@@ -0,0 +1,8 @@
+public class a {
+    public static void main(String[] args) {
+        float h = 1.75f;
+        int   w = 55;
+        float BMI=w/(h*h);
+        System.out.println(BMI);
+    }
+}
