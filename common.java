c9e531853545e8ebda8b044c768dd60f82d03191
yihong45 <93572293+yihong45@users.noreply.github.com>
2021-11-16 16:25:08 +0800

    Add files via upload

---
+++ common.java
@@ -0,0 +1,12 @@
+public class common {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(i%3==0){
+
+            if(i%7==0){
+                System.out.println(i);
+            }
+            }
+        }
+    }
+}
---
+++ number.java
@@ -0,0 +1,16 @@
+public class number
+{
+    public static void main(String[] args) {
+        int i,j;
+        for( i=2;i<=100;i++){
+            for( j=2;j<i;j++){
+            if(i%j==0)
+                break;
+
+            }
+            if(j==i)
+                System.out.println(i);
+        }
+
+    }
+}
